a41761787f27b58ac504c2b97628ff56b58aa124
LMT Helpdesk <mwatstein@umass.edu>
2024-03-13 20:22:59 +0000

    modified:   mk_runs.py

--- mk_runs.py
+++ mk_runs.py
@@ -12,13 +12,13 @@ project="2024-S1-MX-11"
 on = {}
 
 on["J154506"] = \
- [ 111655, 111656, 111657, 111659, 111660, 111661, 111663, 111664, 111665, 111669, 111670, 111671,]
+ [ 111655, 111656, 111657, 111659, 111660, 111661, 111663, 111664, 111665, 111669, 111670, -111671,]
 
 
 #        common parameters per source on the first dryrun (run1a, run2a)
 pars1 = {}
 
-pars1["J154506"] = ""
+pars1["J154506"] = "speczoom=97,4 badcb=1/2"
 
 #        common parameters per source on subsequent runs (run1b, run2b)
 pars2 = {}
